6580ddc718c2b3dc3f0e86a2140dfd4a96a9aeb9
Nada-Mamdouh <nada-mamdouh@outlook.com>
2025-08-24 01:30:08 +0300

    - add models - enums and basic structure

---
+++ Beginner/Task Tracker/Helpers/JsonHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Buffers;
+using myTask = Task_Tracker.Models.Task;
+namespace Task_Tracker.Helpers
+{
+    public static class JsonHelper
+    {
+        const string PATH = "data.json";
+        private static int ID = 0;
+        public static async Task WriteEntityToFile(myTask task)
+        {
+            try
+            {
+                List<myTask> tasks = new();
+
+                if (File.Exists(PATH))
+                {
+                    byte[] rawData = await File.ReadAllBytesAsync(PATH);
+                    using var stream = new MemoryStream(rawData);
+                    if (rawData != null && rawData.Length > 0)
+                    {
+                        tasks = await JsonSerializer.DeserializeAsync<List<myTask>>(stream) ?? new List<myTask>();
+                    }
+                }
+                ///[TODO] add or update or remove task here
+                myTask newToDo = new()
+                {
+                    Id = ++ID,
+                    Description = task.Description,
+                    Status = task.Status,
+                    CreatedAt = DateTime.UtcNow
+                };
+                tasks.Add(newToDo);
+                using var fs = new FileStream(PATH, FileMode.Create, FileAccess.Write, FileShare.None);
+                using var sw = new Utf8JsonWriter(fs, new JsonWriterOptions { Indented = true });
+
+                await JsonSerializer.SerializeAsync(fs,tasks);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception occurred while writing to file, {ex.Message} ",ex);
+            }
+        }
+        private static async Task<int> GetHighestId()
+        {
+            //[TODO]
+            return 1;
+        }
+    }
+}
---
+++ Beginner/Task Tracker/Program.cs
@@ -0,0 +1,34 @@
+﻿using Task_Tracker.Services;
+using myTask = Task_Tracker.Models.Task;
+using myTaskStatus = Task_Tracker.Models.Enums.TaskStatus;
+
+namespace Task_Tracker
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                //Read args - create
+                string appname = AppDomain.CurrentDomain.FriendlyName;
+                Console.WriteLine("Welcome to our to-do list app!");
+                Console.Write(appname + '>');
+                var line = Console.ReadLine();
+                var command = line.Split(" ")[0];
+                var tododesc = line.Split(" ")[1];
+                myTask newTODO = new()
+                {
+                    Description = tododesc,
+                    Status = myTaskStatus.TODO,
+                };
+                await TasksCRUDService.AddTask(newTODO);
+                
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
